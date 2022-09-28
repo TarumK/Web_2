9f359f10b563c12a3ba8d4418c344204ab131f6f
TarumK <kyabishev72@gmail.com>
2022-09-28 12:45:02 +0300

    28092002

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class UserForm(forms.Form):
-    name = forms.CharField(label = "Имя клиента")
-    age = forms.IntegerField(label = "Возраст клиента")
+    name = forms.CharField(label = "Имя клиента", initial="Murat")
+    age = forms.IntegerField(label = "Возраст клиента", initial="20")
     basket = forms.BooleanField(label="Пoлoжить товар в корзину", required=False)
     vyb = forms.NullBooleanField(label="Bы поедете в Сочи этим летом?")
     email = forms.EmailField(label="Введите электронный адрес")
@@ -11,3 +11,4 @@ class UserForm(forms.Form):
     file_path = forms.FilePathField(label="Bыбepитe файл", path="C:/Windows", allow_folders="True", allow_files="True")
     file = forms.FileField(label="Фaйл")
     lang = forms.ChoiceField(choices=((1, "Английский"), (2, "Немецкий"), (3, "Французский")))
+    comment = forms.CharField(label="Koммeнтapий", widget= forms.Textarea)
