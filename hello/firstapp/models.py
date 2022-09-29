82430932d7baebf3a1133c249534a51bded3422d
slkom2 <slkom2@gmail.com>
2022-09-29 16:58:27 +0300

    29_09_2022

--- hello/firstapp/models.py
+++ hello/firstapp/models.py
@@ -5,3 +5,12 @@ class Person(models.Model):
     age = models.IntegerField()
     object = models.Manager()
     DoesNotExist = models.Manager
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+
